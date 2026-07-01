e4128c7742dbb72f841b9c05f0ddbe1ab7d307e9
jong845 <jongtest845@gmail.com>
2026-07-01 14:23:28 +0900

    fix: root 에러 해결

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import './index.css';
-import {RouterProvider} from 'react-router';
-import {router} from './router/routes.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router";
+import { router } from "./router/routes.tsx";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 
 if (!rootElement) {
-  new Error('root 엘리먼트 찾을 수 없음 ㅅㄱ');
+  throw new Error("root 엘리먼트 찾을 수 없음 ㅅㄱ");
 }
 
 createRoot(rootElement).render(
